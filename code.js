095906e1d8b98bd4e1876b7d2d25681a0767110b
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-04-27 12:38:25 +0100

    Add initial implementation of shopping cart functionality

---
+++ code.js
@@ -0,0 +1,36 @@
+let items = []
+
+let countEl = document.getElementById("count")
+let containerEl = document.querySelector(".items-container")
+let inputEl = document.getElementById("input-el")
+
+
+
+
+function additem() {
+     items.push({
+        name: inputEl.value,
+        bought: false
+    })
+    countEl.innerText = "Items: " + items.length
+   
+    renderitem()
+}
+
+
+function bought(index){
+       items[index].bought = !items[index].bought
+
+       renderitem()
+
+}
+
+
+function renderitem(){
+    containerEl.innerHTML = ""
+    for (let i = 0; i < items.length; i++) {
+        containerEl.innerHTML += `<div class="card ${items[i].bought ? 'bought' : ''}"onclick="bought(${i})">
+    <p>${items[i].name}</p>
+</div>`
+    }
+}
